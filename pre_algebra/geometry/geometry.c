2216295d6f17572954b31b1776cd72cdbc5d37f9
codyazari <azaricody@gmail.com>
2022-11-02 08:51:45 -0400

    Updated the geometry.c file to be more extensible.

--- pre_algebra/geometry/geometry.c
+++ pre_algebra/geometry/geometry.c
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-
-float pythagorean_theorem(int choice, float x, float y)
+float pythagorean_theorem(void)
 {
+    int choice;
+
+    float x, y;
+
     float side;
+
+    printf("Press 1 to find the hypotenuse length, 2 to find a side length: \n");
+    scanf("%d", &choice);
+
+    printf("Enter side 1: \n");
+    scanf("%f", &x);
+    printf("Enter side 2: \n");
+    scanf("%f", &y);
 
     switch (choice) {
         case 1:
@@ -17,24 +28,26 @@ float pythagorean_theorem(int choice, float x, float y)
             } else {
                 side = sqrt(pow(x, 2) - pow(y, 2));
             }
-
             return side;
     }
-
-
-
     return side;
+}
+
+void choice()
+{
+    int selection;
+    printf("Select an option from below: \n");
+    printf("1: Pythagorean Theorem\n");
+
+    scanf("%d", &selection);
+    switch (selection) {
+        case 1:
+            printf("%f\n", pythagorean_theorem());
+    }
 }
 
 int main(void)
 {
-    int choice;
-
-    printf("Press 1 to find the hypotenuse length, 2 to find a side length: \n");
-    scanf("%d", &choice);
-
-    printf("%f", pythagorean_theorem(choice, 5, 10));
-
-
+    choice();
     return 0;
 }
